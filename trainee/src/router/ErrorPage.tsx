15142b0e98e052fa5048bf7b217c69728246837a
Olesyavelikaya <Olesyavelikor@gmail.com>
2024-06-15 11:24:48 +0300

    update errorPage

--- trainee/src/router/ErrorPage.tsx
+++ trainee/src/router/ErrorPage.tsx
@@ -1,20 +1,23 @@
 import { Link, useRouteError } from "react-router-dom";
 import "./error.css";
-import "../component/Buttons/Button.css";
 
 export default function ErrorPage() {
   const error = useRouteError();
   return (
     <div className="errorPage">
       <div>
-        <h2>Упс!</h2> Что-то пошло не так!
+        <h2>Упс!</h2> <br /> Что-то пошло не так!
+        <br />
         <i>
           {(error as { statusText: string }).statusText ||
             (error as Error).message}
         </i>
       </div>
+      <br />
       <Link to={"/"}>
-        <button className="button-header">Вернуться на главную</button>
+        <button className="button-error">
+          Вернуться <br /> на главную
+        </button>
       </Link>
     </div>
   );
